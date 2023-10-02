0694e1f4f7f7eb55acf07f57362e0cc257310ccf
Eraikumaran Selvakumaran <erai809@hotmail.com>
2023-10-02 21:51:01 +0200

    replicate train ai model failed model webhook credit fix

--- app/api/replicate/train-model/webhook/route.ts
+++ app/api/replicate/train-model/webhook/route.ts
@@ -10,6 +10,20 @@ export async function POST(request: Request) {
     }
 
     if (status === "canceled" || status === "failed") {
+        const aiModel = await prisma.customAiModel.findUnique({
+            where: { replicate_model_id: id },
+            include: { user: { select: { id: true, credit: true } } },
+        });
+        if (!aiModel || !aiModel.user)
+            return NextResponse.json({ message: "Failed", details: "Failed training model..." });
+
+        if (aiModel.status === "generating") {
+            await prisma.user.update({
+                where: { id: aiModel.user.id },
+                data: { credit: aiModel.user.credit + 50 },
+            });
+        }
+
         const updatedModel = await prisma.customAiModel.update({
             where: { replicate_model_id: id },
             data: { status: "failed" },
